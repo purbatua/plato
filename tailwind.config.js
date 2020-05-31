6cc591132174e348272954b487cab9c9df7554ca
purbatua <purbatua.pasaribu@gmail.com>
2020-06-01 00:17:15 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.vue'],
+  // purge: ['./src/**/*.html', './src/**/*.vue'],
   theme: {
     extend: {
       fontSize: {
